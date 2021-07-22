c43be5b63347405e3d0ff425b4c60f7748b80f7d
Hữu Hà <business@zsmart.tech>
2021-07-22 19:53:56 +0000

    New Deployment

---
+++ precache-manifest.9f873c8530d60c91829730087a84297c.js
@@ -0,0 +1,198 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2c809fe9fbad87857e61511d7ed730f",
+    "url": "/404.html"
+  },
+  {
+    "revision": "4a1ab1594b7bed0e2c2129c37f05a8b1",
+    "url": "/blog/cac-loai-quan-lot-nu-va-lua-chon-phu-hop-khi-mac-1/index.html"
+  },
+  {
+    "revision": "934ea3a40b2e4253a0cf8347b45e5da1",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "b17f1475c9f0b7ebf5b6f5b023d25b17",
+    "url": "/blog/nhung-yeu-cau-ve-mot-chiec-quan-lot-nam-doc-la-2/index.html"
+  },
+  {
+    "revision": "c048e1e3a5b53edf646a",
+    "url": "/css/app.ad2efa93.css"
+  },
+  {
+    "revision": "730ebf9b4530c4985df2",
+    "url": "/css/chunk-07b21386.1fc01d72.css"
+  },
+  {
+    "revision": "76cd1b2258cf457fcfe2",
+    "url": "/css/chunk-29ffb500.0c2b5fde.css"
+  },
+  {
+    "revision": "77cfc247425ca655b1e1",
+    "url": "/css/chunk-2aa8a7e8.664882ff.css"
+  },
+  {
+    "revision": "03cdd6e8145c60f39f77",
+    "url": "/css/chunk-2ef3cdee.9c1188ba.css"
+  },
+  {
+    "revision": "98182600c1f4d7b276a5",
+    "url": "/css/chunk-32d75d88.61d14fb6.css"
+  },
+  {
+    "revision": "d05730fcdb452061be4b",
+    "url": "/css/chunk-619e9930.9757477d.css"
+  },
+  {
+    "revision": "7c753b4971862c597006",
+    "url": "/css/chunk-c339a818.c3c54ec5.css"
+  },
+  {
+    "revision": "6ceec49437c1b16883ba",
+    "url": "/css/chunk-vendors.44bea782.css"
+  },
+  {
+    "revision": "5ebea042851d7d996b014db958913ba6",
+    "url": "/img/banner1.5ebea042.jpg"
+  },
+  {
+    "revision": "23fe69e51ce54817348a2aaab840bebd",
+    "url": "/img/dan-ong-quyen-ru.png"
+  },
+  {
+    "revision": "c48d697cdd2f052b82a9167330cf8bcf",
+    "url": "/img/do-lot-nam-calvin-klein.png"
+  },
+  {
+    "revision": "597d45c5e6161dd6faeb8c78b13ca8bd",
+    "url": "/img/do-lot-nam-ho-van.png"
+  },
+  {
+    "revision": "60ef7fdddb325a2acce7ec39e8079b68",
+    "url": "/img/do-lot-nam-mau.png"
+  },
+  {
+    "revision": "b1d39923f0c019ef960e7becb82044f6",
+    "url": "/img/img_banner_home_1.b1d39923.png"
+  },
+  {
+    "revision": "2cb850545cb1b24dd769b14de6489d0a",
+    "url": "/img/img_banner_home_2.2cb85054.png"
+  },
+  {
+    "revision": "4e151eca70e1a9a549a5e294d5b89b02",
+    "url": "/img/img_banner_home_3.4e151eca.png"
+  },
+  {
+    "revision": "d102490bfeb1015bd080c0f297fd43bd",
+    "url": "/img/img_banner_home_4.d102490b.png"
+  },
+  {
+    "revision": "558bbfb27d314b2833b80370c1ead1f8",
+    "url": "/img/img_banner_home_5.558bbfb2.png"
+  },
+  {
+    "revision": "29e13c1cfbc08c6f6cc6ffa9cc676224",
+    "url": "/img/img_banner_home_6.29e13c1c.png"
+  },
+  {
+    "revision": "654be934e48596af27bee94215696f2e",
+    "url": "/img/logo.654be934.png"
+  },
+  {
+    "revision": "92acebecb105f214cc0363c03484619d",
+    "url": "/img/quan-lo-nu-cap-cao-den.png"
+  },
+  {
+    "revision": "843d51b3ee4e2a3a5547e54ff3071b78",
+    "url": "/img/quan-lot-giay-nu.png"
+  },
+  {
+    "revision": "b4149f04bab9c73b340827f4863327e3",
+    "url": "/img/quan-lot-nam-bai-viet-1.jpg"
+  },
+  {
+    "revision": "529fe0da1a3a315d2cc7cd5df6a1a835",
+    "url": "/img/quan-lot-nu-MiiOW-sieu-mong.png"
+  },
+  {
+    "revision": "5f9c516aa02c2d45ed435f174184b044",
+    "url": "/img/quan-lot-nu-bai-viet-2.jpg"
+  },
+  {
+    "revision": "f31943151323b5884b3d1dbd7c5cd3be",
+    "url": "/img/quan-lot-nu-khoet-day.png"
+  },
+  {
+    "revision": "dccf7d2c2e416164765136460300950b",
+    "url": "/img/quan-lot-nu-muji-xuat-Nhat.png"
+  },
+  {
+    "revision": "451b1293cbfaffaa4b30830378cfc037",
+    "url": "/img/quan-lot-nu-ren-luoi.png"
+  },
+  {
+    "revision": "dafc03a625b77bf3686b41c09a56da87",
+    "url": "/img/quan-lot-nu-thun-lanh.png"
+  },
+  {
+    "revision": "1c833d797bec3aa20e3a3402d0034f51",
+    "url": "/img/quan-lot-nu-trong-suot-xuyen-thau.png"
+  },
+  {
+    "revision": "b5678d3083f3688f3576a400920dc2cc",
+    "url": "/img/section_6_1.b5678d30.jpg"
+  },
+  {
+    "revision": "ebbb9858c79ba646cd8978d2b6be8b81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c048e1e3a5b53edf646a",
+    "url": "/js/app.2bad9990.js"
+  },
+  {
+    "revision": "730ebf9b4530c4985df2",
+    "url": "/js/chunk-07b21386.5f1258a0.js"
+  },
+  {
+    "revision": "76cd1b2258cf457fcfe2",
+    "url": "/js/chunk-29ffb500.aed4f216.js"
+  },
+  {
+    "revision": "77cfc247425ca655b1e1",
+    "url": "/js/chunk-2aa8a7e8.b54e0033.js"
+  },
+  {
+    "revision": "03cdd6e8145c60f39f77",
+    "url": "/js/chunk-2ef3cdee.bfb9b803.js"
+  },
+  {
+    "revision": "98182600c1f4d7b276a5",
+    "url": "/js/chunk-32d75d88.1e5f0f70.js"
+  },
+  {
+    "revision": "d05730fcdb452061be4b",
+    "url": "/js/chunk-619e9930.99f5fade.js"
+  },
+  {
+    "revision": "7c753b4971862c597006",
+    "url": "/js/chunk-c339a818.f54f9172.js"
+  },
+  {
+    "revision": "6ceec49437c1b16883ba",
+    "url": "/js/chunk-vendors.a9f70fe4.js"
+  },
+  {
+    "revision": "1cdf7452dd86225cf857b7d9b0ae5ee1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "1594d288496262d77fe9ce4b7acfdf0c",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d91e00a54cf0f1df2802de08e287ae23",
+    "url": "/sitemap.txt"
+  }
+]);
